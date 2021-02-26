bc6651a52138e7a4a644a39474542694a2c22efc
Nicholas V Frangione <nfrangione8@gmail.com>
2021-02-26 10:08:30 -0800

    commit tree 114fa72e3521b4915b4ceabca20920452038c248

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(fun = 'roller-skate') {
+    return `This Saturday, I want to ${fun}!`
+}
+
+function mondayWork(work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(wrapping = "*") {
+    let message = `You are `
+    return function(descriptor = "special") {
+        return `You are ${wrapping}${descriptor}${wrapping}!`
+    }
+}
